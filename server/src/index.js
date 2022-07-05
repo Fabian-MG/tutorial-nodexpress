17ab9cce4aaec89f3c67c3aa2efbea9fd591882a
Fabian-MG <mirandafabian1510@gmail.com>
2022-07-05 13:56:44 -0500

    Modificando index.js para servir aplicación de react

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,43 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const { loadPlanetData } = require('./models/planets.model');
 
 const planetsRouter = require('./routes/planets.route');
 
+const whitelist = ['http://localhost:3000']
+
+const publicPath = path.join(__dirname, '..', 'public');
+const indexPath = path.join(__dirname, '..', 'public', 'index.html')
+
 const PORT = 8000;
 
 const app = express();
 
+app.use(cors({
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== 1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}))
+
 app.use(express.json());
 
+app.use(express.static(publicPath));
+
 app.use('/planets', planetsRouter);
+
+app.get('/*', (req, res, next) => {
+    try {
+        res.sendFile(indexPath);
+    } catch(err) {
+        console.log('Error al servir cliente');
+    }
+})
 
 async function startServer() {
     await loadPlanetData();
@@ -24,4 +51,4 @@ async function startServer() {
     })
 }
 
-startServer()
+startServer();
